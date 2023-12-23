5c5e762595f7255c5a8c97ebd5b1f2d357443069
Luan Nguyen <hieuluan.2109@gmail.com>
2023-12-23 23:04:49 +0700

    fix: responsive

--- src/components/GoodsCard/index.jsx
+++ src/components/GoodsCard/index.jsx
@@ -7,7 +7,7 @@ const GoodsCard = ({ itemDetail = {}, showAddToCardButton = false, styles = {} }
 
 	return (
 		<div className={clsx("p-1 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:bg-white-500 duration-300 max-w-[50%] sm:max-w-[33.33%] md:max-w-[20%] basis-1/2 sm:basis-1/3 md:basis-1/5", styles?.root)} onClick={() => router.push(`/shop/${itemDetail?.name || 'airpod'}`)}>
-			<div className={clsx("pb-4 items-center mb-1 relative flex h-60 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md ", styles?.ctn)}>
+			<div className={clsx("pb-1 items-center mb-1 relative flex h-60 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md ", styles?.ctn)}>
 				<div className="w-full relative mx-4 h-96 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
 					<img
 						src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
@@ -29,7 +29,9 @@ const GoodsCard = ({ itemDetail = {}, showAddToCardButton = false, styles = {} }
 				</div>
 				{
 					showAddToCardButton && 
-						<button className='w-fit h-[32px] text-xs rounded-lg border border-red-500 py-1 px-4'>Thêm vào giỏ hàng</button>
+						<div className='px-4 mb-1'>
+							<button className='w-fit text-xs rounded-lg border border-red-500 py-1 px-2'>Thêm vào giỏ hàng</button>
+						</div>
 				}
 			</div>
 		</div>
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -27,14 +27,14 @@ const Sidebar = () => {
 				<span className="block w-4 h-0.5 bg-gray-700 mb-1"></span>
 			</button>
 			<div className={`${isOpen ? 'block' : 'hidden'} overscroll-contain top-0 left-0 z-10 w-full fixed h-full md:block md:w-auto transform  translate-x-0 transition-transform ease-in-out duration-300 flex flex-row`} id="side-menu">
-				<ul className="bg-black text-white w-3/5 h-full font-medium flex flex-col p-4 md:p-0  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
-					<SearchBar navClass="!block !p-0 !mt-6 !mb-6" inputClass="p" />
+				<ul className="bg-black text-white w-4/5 h-full font-medium flex flex-col p-4 md:p-0  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
+					<SearchBar navClass="!block !p-0 !mt-6 !mb-6" btnClass="" />
 					<div className=" text-gray-300 border-b border-t border-zinc-500 py-3" >Giấy cuốn</div>
 					<div className=" text-gray-300 border-b border-zinc-500 py-3" aria-current="page">Tẩu thuốc</div>
 					<div className=" text-gray-300 border-b border-zinc-500 py-3" aria-current="page">Bật lửa</div>
 					<div className=" text-gray-300 border-b border-zinc-500 py-3"aria-current="page">Hóa hơi</div>
 				</ul>
-				<div onClick={() => handleOpenMenu(false)} className="w-2/5 h-full bg-red-500  opacity-70">
+				<div onClick={() => handleOpenMenu(false)} className="w-1/5 h-full bg-red-500  opacity-70">
 					<svg className="ml-auto h-8 w-8 text-white"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
 				</div>
 			</div>
